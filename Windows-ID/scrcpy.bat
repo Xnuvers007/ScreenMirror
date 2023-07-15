bd6460e0e4053cd0eac7256571f66cd73dd12c50
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2023-07-15 16:42:12 +0700

    Update scrcpy.bat

--- Windows-ID/scrcpy.bat
+++ Windows-ID/scrcpy.bat
@@ -5,8 +5,10 @@ Title Screen Mirroring by Xnuvers007
 REM Periksa apakah 32-bit atau 64-bit
 if "%PROCESSOR_ARCHITECTURE%"=="x86" (
     set "scrcpyPath=C:\scrcpy-win32-v2.1.1\scrcpy-win32-v2.1.1"
+    echo Anda menggunakan laptop 32-bit.
 ) else (
     set "scrcpyPath=C:\scrcpy-win64-v2.1.1\scrcpy-win64-v2.1.1"
+    echo Anda menggunakan laptop 64-bit.
 )
 
 REM Tambahkan direktori scrcpy ke PATH
